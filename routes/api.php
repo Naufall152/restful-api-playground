49c33c4a86b0f0998a86181eadd4e125b2003ee3
Naufall152 <ahmadianto586@gmail.com>
2025-04-11 16:40:43 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -11,17 +11,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-// Endpoint login (tidak memerlukan token)
 Route::post('/login', [AuthController::class, 'login']);
-
-// Group endpoint yang dilindungi dengan JWT
 Route::middleware('auth:api')->group(function () {
-    // Info user yang sedang login
     Route::get('/me', [AuthController::class, 'me']);
-
-    // Endpoint logout (memerlukan token)
     Route::post('/logout', [AuthController::class, 'logout']);
-
-    // Endpoint student CRUD yang diproteksi token
     Route::apiResource('/students', StudentController::class);
 });
